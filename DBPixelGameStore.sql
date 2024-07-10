397aa53b0087825d0ef1133d11bc8f759f015f6c
kcastillo-2021184 <kcastillo-2021184@kinal.edu.gt>
2024-07-09 21:00:27 -0600

    Se agrega script vacio de la BD del proyecto

---
+++ DBPixelGameStore.sql
@@ -0,0 +1,15 @@
+/*
+   	Programador: Kevin Omar Castillo Calderón
+    Carné: 2021184
+    Fila #1
+    Fecha de creación: 
+		17-04-2024
+    Fechas de Modificaciones:
+		02/07/2024
+*/
+
+drop database if exists DBPixelGameStore;
+create database DBPixelGameStore;
+
+use DBPixelGameStore;
+
