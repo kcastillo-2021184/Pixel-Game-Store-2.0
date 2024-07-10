acbfdbce942a7bd8aac0d0c246a4652f0213270f
kcastillo-2021184 <kcastillo-2021184@kinal.edu.gt>
2024-07-09 21:23:21 -0600

    Se agrega script de la BD del proyecto con la primera entidad (Categoria)

--- DBPixelGameStore.sql
+++ DBPixelGameStore.sql
@@ -6,10 +6,20 @@
 		17-04-2024
     Fechas de Modificaciones:
 		02/07/2024
+        09/07/2024
 */
 
 drop database if exists DBPixelGameStore;
 create database DBPixelGameStore;
 
 use DBPixelGameStore;
+
+create table Categoria (
+    codigoCategoria int auto_increment not null,
+    nombreCategoria varchar(30) not null,
+    descripcionCategoria varchar(45) not null,
+    localizacionCategoria varchar(30) not null,
+    primary key (codigoCategoria)
+);
+
 
