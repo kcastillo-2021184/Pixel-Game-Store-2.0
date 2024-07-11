1cc8e7dec0001392ba1d6b0d7c2b3d5b6f27def5
cbarrera-2020306 <cbarrera-2020306@kinal.edu.gt>
2024-07-10 21:22:50 -0600

    Se agrega script de la BD con la séptima entidad (Productos)

--- DBPixelGameStore.sql
+++ DBPixelGameStore.sql
@@ -6,6 +6,7 @@
 		17-04-2024
     Fechas de Modificaciones:
 		02/07/2024
+        09/07/2024
 */
 
 drop database if exists DBPixelGameStore;
@@ -13,3 +14,76 @@ create database DBPixelGameStore;
 
 use DBPixelGameStore;
 
+create table Categoria (
+    codigoCategoria int auto_increment not null,
+    nombreCategoria varchar(30) not null,
+    descripcionCategoria varchar(45) not null,
+    localizacionCategoria varchar(30) not null,
+    primary key (codigoCategoria)
+);
+
+create table Distribuidores (
+    codigoProveedor int auto_increment not null,
+    nombreDistribuidor varchar(60) not null,
+    direccionDistribuidor varchar(150) not null,
+    codigoPostal varchar(5) not null,
+    paginaWeb varchar(50) not null,
+    primary key (codigoProveedor)
+);
+
+create table Clientes (
+    codigoCliente int not null auto_increment,
+    NITCliente varchar(10) not null,
+    nombresCliente varchar(50) not null,
+    apellidosCliente varchar(50) not null,
+    direccionCliente varchar(150) not null,
+    telefonoCliente varchar(8) not null,
+    correoCliente varchar(60) not null,
+    primary key (codigoCliente)
+);
+
+create table Compras (
+    numeroDocumento int not null,
+    fechaDocumento date not null,
+    descripcion varchar(60) not null,
+    totalDocumento decimal(10,2) default 0,
+    primary key (numeroDocumento)
+);
+
+create table CargoEmpleado (
+    codigoCargoEmpleado int not null auto_increment,
+    nombreCargo varchar(45) not null,
+    descripcionCargo varchar(45) not null,
+    turno varchar(15) not null,
+    primary key (codigoCargoEmpleado)
+);
+
+create table TelefonoDistribuidor (
+    codigoTelefonoDistribuidor int not null auto_increment,
+    numeroPrincipal varchar(8) not null,
+    numeroSecundario varchar(8),
+    observaciones varchar(45),
+    codigoDistribuidor int not null,
+    primary key (codigoTelefonoDistribuidor),
+    constraint FK_TelefonoDistribuidor_Distribuidores 
+        foreign key (codigoDistribuidor) references Distribuidores(codigoProveedor)
+);
+
+create table Productos (
+    codigoProducto varchar(15) not null,
+    descripcionProducto varchar(45) not null,
+    precioUnitario decimal(10,2) default 0.00,
+    precioDocena decimal(10,2) default 0.00,
+    precioMayor decimal(10,2) default 0.00,
+    vistaPrevia longblob,
+    existencia int default 0,
+    codigoCategoria int not null,
+    codigoDistribuidor int not null,
+    primary key (codigoProducto),
+    constraint FK_Productos_Categoria
+        foreign key (codigoCategoria)
+            references Categoria(codigoCategoria),
+    constraint FK_Productos_Distribuidores
+        foreign key (codigoDistribuidor)
+            references Distribuidores(codigoProveedor)    
+);
