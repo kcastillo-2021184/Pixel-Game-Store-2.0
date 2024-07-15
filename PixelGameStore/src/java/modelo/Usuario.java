7455d20b6d29f97d380802a5a4d48b5cdd4da8d8
jaceituno-2020037 <jaceituno-2020037@kinal.edu.gt>
2024-07-14 19:22:03 -0600

    Se agrega modelo y modeloDAO de la entidad (Compras) al proyecto

---
+++ PixelGameStore/src/java/modelo/Usuario.java
@@ -0,0 +1,73 @@
+package modelo;
+
+public class Usuario {
+    private int codigoUsuario;
+    private String nombreUsuario;
+    private String apellidoUsuario;
+    private String usuarioLogin;
+    private String contrasena;
+    private int codigoTipoUsuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(int codigoUsuario, String nombreUsuario, String apellidoUsuario, String usuarioLogin, String contrasena, int codigoTipoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.apellidoUsuario = apellidoUsuario;
+        this.usuarioLogin = usuarioLogin;
+        this.contrasena = contrasena;
+        this.codigoTipoUsuario = codigoTipoUsuario;
+    }
+
+    public int getCodigoUsuario() {
+        return codigoUsuario;
+    }
+
+    public void setCodigoUsuario(int codigoUsuario) {
+        this.codigoUsuario = codigoUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
+    }
+
+    public void setApellidoUsuario(String apellidoUsuario) {
+        this.apellidoUsuario = apellidoUsuario;
+    }
+
+    public String getUsuarioLogin() {
+        return usuarioLogin;
+    }
+
+    public void setUsuarioLogin(String usuarioLogin) {
+        this.usuarioLogin = usuarioLogin;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getCodigoTipoUsuario() {
+        return codigoTipoUsuario;
+    }
+
+    public void setCodigoTipoUsuario(int codigoTipoUsuario) {
+        this.codigoTipoUsuario = codigoTipoUsuario;
+    }
+    
+    
+    
+}
