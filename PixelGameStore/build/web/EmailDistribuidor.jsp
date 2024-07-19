9cb84b2756162a432143535d80da02fc82c39be8
dcaal-2020531 <dcaal-2020531@kinal.edu.gt>
2024-07-19 10:58:55 -0600

    Se agrega CRUD completo al modelo DAO de la entidad(EmailDistribuidor)

---
+++ PixelGameStore/build/web/EmailDistribuidor.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Email Distribuidor</title>
+    </head>
+    <body>
+        <h1>Email Distribuidor</h1>
+    </body>
+</html>
--- PixelGameStore/src/java/config/Conexion.java
+++ PixelGameStore/src/java/config/Conexion.java
@@ -3,15 +3,15 @@ package config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class Conexion {
 
+public class Conexion {
     Connection conexion;
     
     public Connection Conexion(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBPixelGameStore?userSSL=flase", "root","admin");
-        }catch (Exception e){
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/DBPixelGameStore?useSSL=false", "root", "admin");
+        }catch(Exception e){
             e.printStackTrace();
         }
         return conexion;
