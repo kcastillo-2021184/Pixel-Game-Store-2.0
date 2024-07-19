283601c5a92f6bec285c2b52c61767028bad11e0
jbarrera-2020316 <jbarrera-2020316@kinal.edu.gt>
2024-07-19 14:54:39 -0600

    Se agrega el crud al modelo TelefonoDistribuidorDAO

---
+++ PixelGameStore/src/java/modelo/Factura.java
@@ -0,0 +1,80 @@
+package modelo;
+
+import java.util.Date;
+
+
+public class Factura {
+    
+    private int numeroFactura;
+    private String estado;
+    private String totalFactura;
+    private Date fechaFactura;
+    private int codigoCliente;
+    private int codigoEmpleado;
+
+    public Factura() {
+    }
+
+    public Factura(int numeroFactura, String estado, String totalFactura, Date fechaFactura, int codigoCliente, int codigoEmpleado) {
+        this.numeroFactura = numeroFactura;
+        this.estado = estado;
+        this.totalFactura = totalFactura;
+        this.fechaFactura = fechaFactura;
+        this.codigoCliente = codigoCliente;
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getTotalFactura() {
+        return totalFactura;
+    }
+
+    public void setTotalFactura(String totalFactura) {
+        this.totalFactura = totalFactura;
+    }
+
+    public Date getFechaFactura() {
+        return fechaFactura;
+    }
+
+    public void setFechaFactura(Date fechaFactura) {
+        this.fechaFactura = fechaFactura;
+    }
+
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public int getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(int codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+    
+    
+    
+    
+    
+    
+}
